dafdbd52f2b67eabc0c74b9ab635fbb457754ddd
root <root@dev.numec.prp.usp.br>
2014-03-24 15:08:17 -0300

    ABC Search Engine Implementation

---
+++ patientregistrationsystem/qdc/quiz/abc_search_engine.py
@@ -0,0 +1,42 @@
+__author__ = ''
+
+import pyjsonrpc
+from abc import ABCMeta, abstractmethod
+
+class ABCSearchEngine:
+
+    __metaclass__ = ABCMeta
+
+    #retorna todos os questionarios armazenados no banco de dados da instancia do LimeSurvey
+    @abstractmethod
+    def findAllQuestionnaires(self):
+        server = pyjsonrpc.HttpClient("http://noel.ime.usp.br/index.php/admin/remotecontrol")
+        session_key = server.get_session_key("mori","u4drrxp963n5")
+        list_survey = server.list_surveys(session_key, None)
+        server.release_session_key(session_key)
+        return list_survey
+
+    #retorna o questionario que tenha o nome especificado (assumir que cada questionario tem um nome unico)
+    #@abstractmethod
+    #def findQuestionnaireByName(self, str_name):pass
+
+    #retorna o questionario que tenha o identiricador especificado
+    #@abstractmethod
+    def findQuestionnaireByID(self, id):
+        server = pyjsonrpc.HttpClient("http://noel.ime.usp.br/index.php/admin/remotecontrol")
+        session_key = server.get_session_key("mori","u4drrxp963n5")
+        list_survey = server.list_surveys(session_key, None)
+        server.release_session_key(session_key)
+        try:
+            survey = (survey for survey in list_survey if survey['sid'] == id).next()
+        except StopIteration:
+            survey = None
+        return survey
+
+# Classe envelope para o API do limesurvey
+class Questionnaires(ABCSearchEngine):
+
+    def findAllQuestionnaires(self):
+        return super(Questionnaires, self).findAllQuestionnaires()
+    def findQuestionnaireByID(self, str_id):
+        return super(Questionnaires, self).findQuestionnaireByID(str_id)
