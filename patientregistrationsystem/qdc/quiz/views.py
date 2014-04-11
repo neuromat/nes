bd9e9d357fd04884cda55339470730d88cf005ac
Yoshio Mori <mori@dev.numec.prp.usp.br>
2014-04-11 15:45:38 -0300

    mudança para o branch de cadastro

--- patientregistrationsystem/qdc/quiz/views.py
+++ patientregistrationsystem/qdc/quiz/views.py
@@ -3,10 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import RequestContext, loader
-from django.shortcuts import render
+from django import forms
+from django.core.context_processors import csrf
 
 
 from quiz.models import FleshToneOption, MaritalStatusOption, SchoolingOption, PaymentOption, ReligionOption
+
+from quiz.forms import PatientForm, PersonalDataForm
 
 
 def pg_home(request):
@@ -15,6 +18,12 @@ def pg_home(request):
     schooling_options = SchoolingOption.objects.all()
     payment_options = PaymentOption.objects.all()
     religion_options = ReligionOption.objects.all()
-    context = {'fleshtone_options': fleshtone_options,'marital_status_options':marital_status_options,'schooling_options':schooling_options,'payment_options':payment_options,'religion_options':religion_options}
+    pd = PersonalDataForm
+    test = "Test"
+    if request.method == "POST":
+        test = request.POST
+        form = PatientForm(request.POST)
+        if form.is_valid():
+            new_patient = form.save(commit=False)
+    context = {'fleshtone_options': fleshtone_options,'marital_status_options':marital_status_options,'schooling_options':schooling_options,'payment_options':payment_options,'religion_options':religion_options,'test':test,'pd':pd}
     return render(request,'quiz/pg_home.html',context)
-
