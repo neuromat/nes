5ffffb08f43911cde463e26df8b09cae92d45a6d
Yoshio Mori <yoshio.mori@hotmail.com>
2014-04-28 15:10:55 -0300

    Refactoring

--- patientregistrationsystem/qdc/quiz/views.py
+++ patientregistrationsystem/qdc/quiz/views.py
@@ -20,19 +20,19 @@ def pg_home(request):
     religion_options = ReligionOption.objects.all()
     #pd = PersonalDataForm
     new_patient = None
-    new_personaldata = None
+    new_personal_data = None
     test = None
     if request.method == "POST":
-        fPacient = PatientForm(request.POST)
+        patient_form = PatientForm(request.POST)
         test = request.POST
-        fPersonalData = PersonalDataForm(request.POST)
-        if fPacient.is_valid(): #and fPersonalData.is_valid():
-            new_patient = fPacient.save()
-            #new_personaldata = fPersonalData.save(commit=False)
-            #new_personaldata.id_patient = new_patient
-            #new_personaldata.city_birth_txt = 'test'
-            #new_personaldata.payment_opt = PaymentOption.objects.filter(payment_txt=request.POST['payment_option'])[0]
-            #new_personaldata.flesh_tone_opt = FleshToneOption.objects.filter(flesh_tone_txt=request.POST['fleshtone_options'])[0]
-            #new_personaldata.gender_opt = FleshToneOption.objects.filter(flesh_tone_txt=request.POST['fleshtone_options'])[0]
-    context = {'fleshtone_options': fleshtone_options,'marital_status_options':marital_status_options,'schooling_options':schooling_options,'payment_options':payment_options,'religion_options':religion_options,'new_patient':new_patient,'new_personaldata':new_personaldata,'test':test}
+        #personal_data_form = PersonalDataForm(request.POST)
+        if patient_form.is_valid(): #and personal_data_form.is_valid():
+            new_patient = patient_form.save()
+            #new_personal_data = personal_data_form.save(commit=False)
+            #new_personal_data.id_patient = new_patient
+            #new_personal_data.city_birth_txt = 'test'
+            #new_personal_data.payment_opt = PaymentOption.objects.filter(payment_txt=request.POST['payment_option'])[0]
+            #new_personal_data.flesh_tone_opt = FleshToneOption.objects.filter(flesh_tone_txt=request.POST['fleshtone_options'])[0]
+            #new_personal_data.gender_opt = FleshToneOption.objects.filter(flesh_tone_txt=request.POST['fleshtone_options'])[0]
+    context = {'fleshtone_options': fleshtone_options,'marital_status_options':marital_status_options,'schooling_options':schooling_options,'payment_options':payment_options,'religion_options':religion_options,'new_patient':new_patient,'new_personal_data':new_personal_data,'test':test}
     return render(request,'quiz/pg_home.html',context)
