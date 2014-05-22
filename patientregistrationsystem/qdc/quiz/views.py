4fe2fe75f77208ccb450ff32db0b3accd15b2a16
crns <crns13@gmail.com>
2014-05-22 11:27:50 -0300

    Merge branch 'DEV-0.1' of neurogit:neuromat into Dev-CC-0.1
    Conflicts:
    	patientregistrationsystem/qdc/quiz/forms.py
    	patientregistrationsystem/qdc/quiz/models.py
    	patientregistrationsystem/qdc/quiz/templates/quiz/pg_home.html
    	patientregistrationsystem/qdc/quiz/views.py

--- patientregistrationsystem/qdc/quiz/views.py
+++ patientregistrationsystem/qdc/quiz/views.py
@@ -8,8 +8,9 @@ from django.template import RequestContext, loader
 from django import forms
 from django.core.context_processors import csrf
 from django.shortcuts import render_to_response
+from datetime import date
 
-from models import Patient, FleshToneOption, MaritalStatusOption, SchoolingOption, PaymentOption, ReligionOption, GenderOption
+from models import Patient, SocialDemographicData, FleshToneOption, MaritalStatusOption, SchoolingOption, PaymentOption, ReligionOption, GenderOption
 from forms import PatientForm, SocialDemographicDataForm, SocialHistoryDataForm
 
 
@@ -49,7 +50,7 @@ def pg_home(request):
                 new_social_history_data.save()
                 new_social_history_data = None
     context = {'gender_options': gender_options, 'new_social_history_data': new_social_history_data,
-               'new_social_demographic_data': new_social_demographic_data,'flesh_tone_options': flesh_tone_options,
+               'new_social_demographic_data': new_social_demographic_data, 'flesh_tone_options': flesh_tone_options,
                'marital_status_options': marital_status_options, 'schooling_options': schooling_options,
                'payment_options': payment_options, 'religion_options': religion_options, 'new_patient': new_patient,
                'new_personal_data': new_personal_data}
@@ -77,11 +78,47 @@ def patients(request):
 
 
 def patient(request, patient_id):
+    flesh_tone_options = FleshToneOption.objects.all()
+    marital_status_options = MaritalStatusOption.objects.all()
+    gender_options = GenderOption.objects.all()
+    schooling_options = SchoolingOption.objects.all()
+    payment_options = PaymentOption.objects.all()
+    religion_options = ReligionOption.objects.all()
+
     p = Patient.objects.get(nr_record=patient_id)
+    if p.dt_birth_txt:
+        dt_birth = str(p.dt_birth_txt.day) + "/" + str(p.dt_birth_txt.month) + "/" + str(p.dt_birth_txt.year)
+    else:
+        dt_birth = ""
+    if p.marital_status_opt_id:
+        marital_status_searched = p.marital_status_opt_id
+    else:
+        marital_status_searched = ""
+    if p.gender_opt_id:
+        gender_searched = p.gender_opt_id
+    else:
+        gender_searched = ""
+
+    try:
+        p_social_demo = SocialDemographicData.objects.get(id_patient_id=patient_id)
+    except SocialDemographicData.DoesNotExist:
+        p_social_demo = ""
+    if p_social_demo:
+        if p_social_demo.religion_opt_id:
+            religion_searched = p_social_demo.religion_opt_id
+        else:
+            religion_searched = ""
+
     context = {'name': p.name_txt, 'cpf': p.cpf_id, 'rg': p.rg_id, 'place_of_birth': p.natural_of_txt,
                'citizenship': p.citizenship_txt, 'street': p.street_txt, 'zipcode': p.zipcode_number,
                'country': p.country_txt, 'state': p.state_txt, 'city': p.city_txt, 'phone': p.phone_number,
                'cellphone': p.cellphone_number, 'email': p.email_txt, 'medical_record': p.medical_record_number,
+               'dt_birth': dt_birth,
+               'flesh_tone_options': flesh_tone_options, 'marital_status_options': marital_status_options,
+               'gender_options': gender_options, 'schooling_options': schooling_options,
+               'payment_options': payment_options, 'religion_options': religion_options,
+               'gender_searched': gender_searched, 'marital_status_searched': marital_status_searched,
+               'religion_opt': religion_searched,
                }
     return render(request, 'quiz/pg_home.html', context)
 
