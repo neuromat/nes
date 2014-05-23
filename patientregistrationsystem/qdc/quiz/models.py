4f37cc1c1ca291e1d8be511e7a230d602fddd8e8
crns <crns13@gmail.com>
2014-05-23 17:17:32 -0300

    Merge branch 'DEV-0.1' of neurogit:neuromat into Dev-CC-0.1
    Conflicts:
    	patientregistrationsystem/qdc/quiz/forms.py
    	patientregistrationsystem/qdc/quiz/models.py
    	patientregistrationsystem/qdc/quiz/templates/quiz/pg_home.html
    	patientregistrationsystem/qdc/quiz/views.py

--- patientregistrationsystem/qdc/quiz/models.py
+++ patientregistrationsystem/qdc/quiz/models.py
@@ -49,50 +49,46 @@ class SchoolingOption(models.Model):
         return self.schooling_txt
 
 
-#class SocialClass(models.Model):
-#    social_class_txt = models.CharField(max_length=50)
-#    points = models.IntegerField
+class AmountCigarettesOption(models.Model):
+    amount_cigarettes_txt = models.CharField(max_length=50)
 
-#    def __unicode__(self):
-#        return self.social_class_txt
+    def __unicode__(self):
+        return self.amount_cigarettes_txt
 
-#    def calculateSocialClass(**keywords):
-#        for kw in keywords.keys():
-#            print kw, "-", keywords[kw]
-#        #para cada item adicionar um valor na pontuacao
 
-#   dicTV = {'TVoption0':'0', 'TVoption1' : '1', 'TVoption2' : '2', 'TVoption3' : '3' , 'TVoption4' : '4'}
-#   dicDVD = {'DVDoption0':'0', 'DVDoption1' : '2', 'DVDoption2' : '2', 'DVDoption3' : '2' , 'DVDoption4' : '2'}
-#   dicRadio = {'RADIOoption0':'0', 'RADIOoption1' : '1', 'RADIOoption2' : '2', 'RADIOoption3' : '3' , 'RADIOoption4' : '4'}
-#   dicBath = {'Bathsoption0':'0', 'Bathsoption1' : '4', 'Bathsoption2' : '5', 'Bathsoption3' : '6' , 'Bathsoption4' : '7'}
-#   dicAuto = {'Autooption0':'0', 'Autooption1' : '4', 'Autooption2' : '7', 'Autooption3' : '9' , 'Autooption4' : '9'}
-#   dicHousemaid = {'Employoption0':'0', 'Employoption1' : '3', 'Employoption2' : '4', 'Employoption3' : '4' , 'Employoption4' : '4'}
-#   dicWashMachine = {'WMoption0':'0', 'WMoption1' : '2', 'WMoption2' : '2', 'WMoption3' : '2' , 'WMoption4' : '2'}
-#   dicRefrigerator = {'Refrigoption0':'0', 'Refrigoption1' : '4', 'Refrigoption2' : '4', 'Refrigoption3' : '4' , 'Refrigoption4' : '4'}
-#   dicFreezer = {'Freezoption0':'0', 'Freezoption1' : '2', 'Freezoption2' : '2', 'Freezoption3' : '2' , 'Freezoption4' : '2'}
+class AlcoholFrequencyOption(models.Model):
+    alcohol_frequency_txt = models.CharField(max_length=100)
 
-#chamada:
-#calculateSocialClass(tvs='1', dvds='2', banheiros='3', ...)
+    def __unicode__(self):
+        return self.alcohol_frequency_txt
+
+
+class AlcoholPeriodOption(models.Model):
+    alcohol_period_txt = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.alcohol_period_txt
+
 
 class Patient(models.Model):
-    cpf_id = models.CharField(max_length=15, unique=True)
-    rg_id = models.CharField(max_length=15)
+    cpf_id = models.CharField(null=True, blank=True, max_length=15, unique=True)
+    rg_id = models.CharField(max_length=15, null=True, blank=True)
     name_txt = models.CharField(max_length=50)
     nr_record = models.AutoField(primary_key=True)
-    medical_record_number = models.CharField(max_length=25)
-    natural_of_txt = models.CharField(max_length=50)
-    citizenship_txt = models.CharField(max_length=50)
-    street_txt = models.CharField(max_length=50)
-    zipcode_number = models.CharField(max_length=12)
-    country_txt = models.CharField(max_length=30)
-    state_txt = models.CharField(max_length=30)
-    city_txt = models.CharField(max_length=30)
-    phone_number = models.CharField(max_length=15)
-    cellphone_number = models.CharField(max_length=15)
-    email_txt = models.EmailField()
-    dt_birth_txt = models.DateField()
-    gender_opt = models.ForeignKey(GenderOption)
-    marital_status_opt = models.ForeignKey(MaritalStatusOption)
+    medical_record_number = models.CharField(max_length=25, null=True, blank=True)
+    natural_of_txt = models.CharField(max_length=50, null=True, blank=True)
+    citizenship_txt = models.CharField(max_length=50, null=True, blank=True)
+    street_txt = models.CharField(max_length=50, null=True, blank=True)
+    zipcode_number = models.CharField(max_length=12, null=True, blank=True)
+    country_txt = models.CharField(max_length=30, null=True, blank=True)
+    state_txt = models.CharField(max_length=30, null=True, blank=True)
+    city_txt = models.CharField(max_length=30, null=True, blank=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    cellphone_number = models.CharField(max_length=15, null=True, blank=True)
+    email_txt = models.EmailField(null=True, blank=True)
+    dt_birth_txt = models.DateField(null=True, blank=True)
+    gender_opt = models.ForeignKey(GenderOption, null=True, blank=True)
+    marital_status_opt = models.ForeignKey(MaritalStatusOption, null=True, blank=True)
 
     class Meta:
         permissions = (
@@ -109,43 +105,87 @@ class Patient(models.Model):
 
 class SocialDemographicData(models.Model):
     id_patient = models.ForeignKey(Patient)
-    religion_opt = models.ForeignKey(ReligionOption)
+    religion_opt = models.ForeignKey(ReligionOption, null=True, blank=True)
     profession_txt = models.CharField(null=True, blank=True, max_length=50)
-    occupation_txt = models.CharField(max_length=50)
-    benefit_gov_bool = models.CharField(max_length=10)
-    payment_opt = models.ForeignKey(PaymentOption)
-    flesh_tone_opt = models.ForeignKey(FleshToneOption)
-    schooling_opt = models.ForeignKey(SchoolingOption)
-    tv_opt = models.IntegerField()
-    dvd_opt = models.IntegerField()
-    radio_opt = models.IntegerField()
-    bath_opt = models.IntegerField()
-    automobile_opt = models.IntegerField()
-    wash_machine_opt = models.IntegerField()
-    refrigerator_opt = models.IntegerField()
-    freezer_opt = models.IntegerField()
-    house_maid_opt = models.IntegerField()
-    # social_class_opt = models.ForeignKey(SocialClass)
+    occupation_txt = models.CharField(null=True, blank=True, max_length=50)
+    benefit_gov_bool = models.CharField(null=True, blank=True, max_length=10)
+    payment_opt = models.ForeignKey(PaymentOption, null=True, blank=True)
+    flesh_tone_opt = models.ForeignKey(FleshToneOption, null=True, blank=True)
+    schooling_opt = models.ForeignKey(SchoolingOption, null=True, blank=True)
+    tv_opt = models.IntegerField(null=True, blank=True, )
+    dvd_opt = models.IntegerField(null=True, blank=True, )
+    radio_opt = models.IntegerField(null=True, blank=True, )
+    bath_opt = models.IntegerField(null=True, blank=True, )
+    automobile_opt = models.IntegerField(null=True, blank=True, )
+    wash_machine_opt = models.IntegerField(null=True, blank=True, )
+    refrigerator_opt = models.IntegerField(null=True, blank=True, )
+    freezer_opt = models.IntegerField(null=True, blank=True, )
+    house_maid_opt = models.IntegerField(null=True, blank=True, )
+    social_class_opt = models.CharField(null=True, blank=True, max_length=10)
 
     def __unicode__(self):
         return \
             self.id_patient, self.religion_opt, self.profession_txt, self.occupation_txt, bool(self.benefit_gov_bool), \
             self.payment_opt, self.flesh_tone_opt, self.schooling_opt, self.tv_opt, self.dvd_opt, self.radio_opt, \
             self.bath_opt, self.automobile_opt, self.wash_machine_opt, self.refrigerator_opt, self.freezer_opt, \
-            self.house_maid_opt
+            self.house_maid_opt, self.social_class_opt
+
+    def calculateSocialClass(self, **keywords):
+        dictDvd = {'0': 0, '1': 2, '2': 2, '3': 2, '4': 2}
+        dictBath = {'0': 0, '1': 4, '2': 5, '3': 6, '4': 7}
+        dictAuto = {'0': 0, '1': 4, '2': 7, '3': 9, '4': 9}
+        dictHousemaid = {'0':0, '1': 3, '2': 4, '3': 4, '4': 4}
+        dictRefrigerator = {'0': 0, '1': 4, '2': 4, '3': 4, '4': 4}
+        dicScolarity = {'Analfabeto/ até 3ª Série Fundamental': 0, '4ª Fundamental': 1, 'Fundamental Completo': 2, 'Médio Completo': 4, 'Superior Completo': 8}
+        points = 0
+
+        for kw in keywords.keys():
+            if kw == 'tv' or kw == 'radio':
+                points += int(keywords[kw])
+            elif kw == 'banheiro':
+                points += dictBath[keywords[kw]]
+            elif kw == 'automovel':
+                points += dictAuto[keywords[kw]]
+            elif kw == 'empregada':
+                points += dictHousemaid[keywords[kw]]
+            elif kw == 'dvd' or kw == 'maquina' or kw == 'freezer':
+                points += dictDvd[keywords[kw]]
+            elif kw == 'geladeira':
+                points += dictRefrigerator[keywords[kw]]
+            elif kw == 'escolaridade':
+                points += dicScolarity[keywords[kw]]
+
+        if points >= 0 and points <= 7:
+            return 'E'
+        elif points >= 8 and points <= 13:
+            return 'D'
+        elif points >= 14 and points <= 17:
+            return 'C2'
+        elif points >= 18 and points <= 22:
+            return 'C1'
+        elif points >= 23 and points <= 28:
+            return 'B2'
+        elif points >= 29 and points <= 34:
+            return 'B1'
+        elif points >= 35 and points <= 41:
+            return 'A2'
+        elif points >= 42 and points <= 46:
+            return 'A1'
+        else:
+            return ''
 
 
 class SocialHistoryData(models.Model):
     id_patient = models.ForeignKey(Patient)
-    smoker = models.CharField(max_length=10)
-    amount_cigarettes_opt = models.CharField(max_length=25)
-    ex_smoker = models.CharField(max_length=10)
-    alcoholic = models.CharField(max_length=10)
-    alcohol_frequency = models.CharField(max_length=25)
-    alcohol_period_opt = models.CharField(max_length=25)
-    drugs_opt = models.CharField(max_length=25)
+    smoker = models.CharField(max_length=10, null=True, blank=True)
+    amount_cigarettes_opt = models.ForeignKey(AmountCigarettesOption, null=True, blank=True, default=0)
+    ex_smoker = models.CharField(max_length=10, null=True, blank=True)
+    alcoholic = models.CharField(max_length=10, null=True, blank=True)
+    alcohol_frequency_opt = models.ForeignKey(AlcoholFrequencyOption, null=True, blank=True, default=0)
+    alcohol_period_opt = models.ForeignKey(AlcoholPeriodOption, null=True, blank=True, default=0)
+    drugs_opt = models.CharField(max_length=25, null=True, blank=True)
 
     def __unicode__(self):
         return \
             self.id_patient, bool(self.smoker), self.amount_cigarettes_opt, bool(self.ex_smoker), bool(self.alcoholic),\
-            self.alcohol_frequency, self.alcohol_period_opt, self.drugs_opt
+            self.alcohol_frequency_opt, self.alcohol_period_opt, self.drugs_opt
