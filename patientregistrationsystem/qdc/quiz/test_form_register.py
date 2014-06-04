fee2f928a660c89ca6a8fbab0e2d5ec854b206d4
erocha <evandro2001@hotmail.com>
2014-06-04 20:05:17 -0300

    Add comments in the tests.

--- patientregistrationsystem/qdc/quiz/test_form_register.py
+++ patientregistrationsystem/qdc/quiz/test_form_register.py
@@ -5,17 +5,20 @@ from django.contrib.auth.models import *
 class FormValidation(TestCase):
 
     def test_invalid_cpf(self):
-        '''testa inclusao de paciente com cpf invalido'''
+        """testa inclusao de paciente com cpf invalido"""
 
-        user = User.objects.create_user(username = 'myadmin', email ='test@dummy.com', password = 'mypwd')
-        uid = user.id
+        username_dummy = 'myadmin'
+        password_dummy = 'mypassword'
+
+        user = User.objects.create_user(username=username_dummy, email='test@dummy.com', password=password_dummy)
         user.is_staff = True
         user.save()
 
-        logged = self.client.login(username='myadmin', password='mypwd')
+        logged = self.client.login(username=username_dummy, password=password_dummy)
         self.assertEqual(logged, True)
 
-        data = {'name_txt': 'outro novo 2', 'cpf_id': '100.913.651-81'}
+        data = {'name_txt': 'Novo paciente',
+                'cpf_id': '100.913.651-81'}
         response = self.client.post('/quiz/register/', data)
         self.assertEqual(response.status_code, 200)
         self.assertFormError(response, "patient_form", "cpf_id", u'CPF 100.913.651-81 n\xe3o \xe9 v\xe1lido')
--- patientregistrationsystem/qdc/quiz/test_validation.py
+++ patientregistrationsystem/qdc/quiz/test_validation.py
@@ -2,9 +2,10 @@ from django.test import TestCase
 
 from validation import CPF
 
-class CPF_validation_test(TestCase):
 
-    goodValues = (
+class CpfValidationTest(TestCase):
+
+    good_values = (
         '288.666.827-30',
         '597.923.110-25',
         '981.108.954-09',
@@ -12,7 +13,7 @@ class CPF_validation_test(TestCase):
         '774.321.431-10',
     )
 
-    goodOnlyNumbersValues = (
+    good_only_numbers_values = (
         '28866682730',
         '59792311025',
         '98110895409',
@@ -20,7 +21,7 @@ class CPF_validation_test(TestCase):
         '77432143110',
     )
 
-    badValues = (
+    bad_values = (
         '288.666.827-31',
         '597.923.110-26',
         '981.108.954-00',
@@ -28,7 +29,7 @@ class CPF_validation_test(TestCase):
         '774.321.431-11',
     )
 
-    badOnlyNumbersValues = (
+    bad_only_numbers_values = (
         '28866682731',
         '59792311026',
         '98110895400',
@@ -36,54 +37,46 @@ class CPF_validation_test(TestCase):
         '77432143111',
     )
 
-
     def test_good_values(self):
-        '''testa os valores validos'''
-        for cpf in self.goodValues:
+        """testa os valores validos"""
+        for cpf in self.good_values:
             result = CPF(cpf).isValid()
             self.assertEqual(result, True)
-
 
     def test_good_only_numbers_values(self):
-        '''testa os valores validos para somente numeros'''
-        for cpf in self.goodOnlyNumbersValues:
+        """testa os valores validos para somente numeros"""
+        for cpf in self.good_only_numbers_values:
             result = CPF(cpf).isValid()
             self.assertEqual(result, True)
 
-
     def test_bad_values(self):
-        '''testa os valores invalidos'''
-        for cpf in self.badValues:
+        """testa os valores invalidos"""
+        for cpf in self.bad_values:
             result = CPF(cpf).isValid()
             self.assertEqual(result, False)
-
 
     def test_bad_only_numbers_values(self):
-        '''testa os valores invalidos para somente numeros'''
-        for cpf in self.badOnlyNumbersValues:
+        """testa os valores invalidos para somente numeros"""
+        for cpf in self.bad_only_numbers_values:
             result = CPF(cpf).isValid()
             self.assertEqual(result, False)
 
-
     def test_empty_value(self):
-        '''testa cpf vazio '''
+        """testa cpf vazio """
         result = CPF('').isValid()
         self.assertEqual(result, False)
 
-
     def test_alpha_value(self):
-        '''testa cpf com letras'''
+        """testa cpf com letras"""
         result = CPF('111.ABC').isValid()
         self.assertEqual(result, False)
 
-
     def test_special_character_value(self):
-        '''testa cpf com letras'''
+        """testa cpf com letras"""
         result = CPF('!@#$%&*()-_=+[]|"?><;:').isValid()
         self.assertEqual(result, False)
 
-
     def test_long_string_value(self):
-        '''testa cpf com letras'''
+        """testa cpf com letras"""
         result = CPF('123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890').isValid()
         self.assertEqual(result, False)
