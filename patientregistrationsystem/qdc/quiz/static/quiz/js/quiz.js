7a717d33b683da4f7cce01cf5b6e0f7d045be571
erocha <evandro2001@hotmail.com>
2014-06-09 09:25:48 -0300

    Changed modal dialog to a toast message when then name is not filled.

--- patientregistrationsystem/qdc/quiz/static/quiz/js/quiz.js
+++ patientregistrationsystem/qdc/quiz/static/quiz/js/quiz.js
@@ -17,8 +17,20 @@ function showErrorMessage(message) {
     toastr.error(message);
 }
 
+function showErrorMessageTemporary(message) {
+    toastr.options.closeButton = true;
+    toastr.options.timeOut = 10000;
+    toastr.error(message);
+}
+
 function showInfoMessage(message) {
     toastr.options.closeButton = true;
     toastr.options.timeOut = 0;
     toastr.info(message);
 }
+
+function jumpToElement(h) {
+    var top = document.getElementById(h).offsetTop;
+    window.scrollTo(0, top);
+}
+
