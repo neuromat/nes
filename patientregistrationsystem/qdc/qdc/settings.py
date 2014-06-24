177ae1471e1d73e3eaf0883852563028fe2f0e0c
erocha <evandro2001@hotmail.com>
2014-06-24 16:37:10 -0300

    Configuration for jenkins

--- patientregistrationsystem/qdc/qdc/settings.py
+++ patientregistrationsystem/qdc/qdc/settings.py
@@ -38,6 +38,19 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'quiz',
+    'django_jenkins',
+)
+
+JENKINS_TASKS = (
+    'django_jenkins.tasks.with_coverage',
+    'django_jenkins.tasks.django_tests',   # select one django or
+    #'django_jenkins.tasks.dir_tests'      # directory tests discovery
+    'django_jenkins.tasks.run_pep8',
+    #'django_jenkins.tasks.run_pyflakes',
+    #'django_jenkins.tasks.run_jslint',
+    #'django_jenkins.tasks.run_csslint',
+    #'django_jenkins.tasks.run_sloccount',
+    #'django_jenkins.tasks.lettuce_tests',
 )
 
 MIDDLEWARE_CLASSES = (
