d2d37373cbc3d9265689c1efb5f6e3f2046f457b
erocha <evandro2001@hotmail.com>
2014-06-24 16:58:14 -0300

    Configuration for jenkins

--- patientregistrationsystem/qdc/qdc/settings.py
+++ patientregistrationsystem/qdc/qdc/settings.py
@@ -45,7 +45,7 @@ JENKINS_TASKS = (
     'django_jenkins.tasks.with_coverage',
     #'django_jenkins.tasks.django_tests',   # select one django or
     #'django_jenkins.tasks.dir_tests'      # directory tests discovery
-    'django_jenkins.tasks.run_pep8',
+    #'django_jenkins.tasks.run_pep8',
     #'django_jenkins.tasks.run_pyflakes',
     #'django_jenkins.tasks.run_jslint',
     #'django_jenkins.tasks.run_csslint',
