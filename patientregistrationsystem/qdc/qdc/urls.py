4a7d3f3172ad46329e9e66ca94632ce595241b1f
Yoshio Mori <yoshio.mori@hotmail.com>
2014-07-02 18:27:05 -0300

    User change your own password.

--- patientregistrationsystem/qdc/qdc/urls.py
+++ patientregistrationsystem/qdc/qdc/urls.py
@@ -13,7 +13,7 @@ urlpatterns = patterns('',
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', {'login_url': '/quiz'}),
     url(r'^password_change/$', 'django.contrib.auth.views.password_change',
-        {'template_name': 'registration/change_password_custom.html'}),
+        {'template_name': 'registration/change_password_custom.html',
+         'post_change_redirect': 'search_patient'}),
     url(r'^password_change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
-
 )
