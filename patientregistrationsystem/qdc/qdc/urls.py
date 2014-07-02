89208349e1b495fefabfbf4dcb7c4fbdce9fc3ae
erocha <evandro2001@hotmail.com>
2014-07-02 17:15:06 -0300

    Password change form (partial)

--- patientregistrationsystem/qdc/qdc/urls.py
+++ patientregistrationsystem/qdc/qdc/urls.py
@@ -12,5 +12,8 @@ urlpatterns = patterns('',
     url(r'^quiz/', include('quiz.urls')),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', {'login_url': '/quiz'}),
+    url(r'^password_change/$', 'django.contrib.auth.views.password_change',
+        {'template_name': 'registration/change_password_custom.html'}),
+    url(r'^password_change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
 
 )
