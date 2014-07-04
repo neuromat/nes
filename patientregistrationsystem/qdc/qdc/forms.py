793350ee52c6895c46319d4e1f036582f0967c48
Yoshio Mori <yoshio.mori@hotmail.com>
2014-07-04 10:55:33 -0300

    User change your own password update and messages fix.

--- patientregistrationsystem/qdc/qdc/forms.py
+++ patientregistrationsystem/qdc/qdc/forms.py
@@ -1,13 +1,16 @@
 __author__ = 'mori'
 from django.contrib.auth.forms import PasswordChangeForm
-from django.forms import CharField, PasswordInput
-from django.utils.translation import ugettext_lazy as _
+from django.forms import PasswordInput
 
 
 class PasswordChangeFormCustomized(PasswordChangeForm):
-    old_password = CharField(label=_("Password"),
-                             widget=PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Entrar senha'}))
-    new_password1 = CharField(label=_("Password confirmation"),
-                              widget=PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Entrar senha'}))
-    new_password2 = CharField(label=_("Old password"),
-                              widget=PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Entrar senha'}))
+    def __init__(self, *args, **kwargs):
+        super(PasswordChangeForm, self).__init__(*args, **kwargs)
+        self.fields['old_password'].widget = PasswordInput(attrs={'class': 'form-control',
+                                                                  'placeholder': 'Entrar senha'})
+        self.fields['new_password1'].widget = PasswordInput(attrs={'class': 'form-control',
+                                                                   'placeholder': 'Entrar senha',
+                                                                   'onkeyup': "password_strong();"})
+        self.fields['new_password2'].widget = PasswordInput(attrs={'class': 'form-control',
+                                                                   'placeholder': 'Entrar senha',
+                                                                   'onkeyup': "checkPass();"})
