a2cd688215693b1a6dd38d9c632bef5fa77cb0df
larissamoraes <larissam@ime.usp.br>
2014-07-31 14:38:22 -0300

    Removed unused functions.

--- patientregistrationsystem/qdc/quiz/static/quiz/js/register.js
+++ patientregistrationsystem/qdc/quiz/static/quiz/js/register.js
@@ -169,30 +169,6 @@ $(document).ready(function () {
 
 });
 
-
-function showDetails(diagnosis_id)
-{
-    $("#description-label-"+diagnosis_id).show();
-    $("#date-label-"+diagnosis_id).show();
-    $("#description-"+diagnosis_id).show();
-    $("#date-"+diagnosis_id).show();
-    $("#details-"+diagnosis_id).hide();
-    $("#confirm-"+diagnosis_id).show();
-
-}
-
-function hideDetails(diagnosis_id)
-{
-    $("#description-label-"+diagnosis_id).hide();
-    $("#date-label-"+diagnosis_id).hide();
-    $("#description-"+diagnosis_id).hide();
-    $("#date-"+diagnosis_id).hide();
-    $("#details-"+diagnosis_id).show();
-    $("#confirm-"+diagnosis_id).hide();
-//    $("#form_medical_record").submit();
-
-}
-
 function validateEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
