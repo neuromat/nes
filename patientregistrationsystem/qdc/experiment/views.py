c6d6b0f9ccd504f5d1601f83bde1762cc06dbc55
romulo <romulojosefranco@gmail.com>
2014-08-14 17:50:40 -0300

    Functionality to insert/remove subjects into experiment.

--- patientregistrationsystem/qdc/experiment/views.py
+++ patientregistrationsystem/qdc/experiment/views.py
@@ -3,15 +3,19 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response
 
-from experiment.models import Experiment, Questionnaire
+from experiment.models import Experiment, Questionnaire, Subject
 from experiment.forms import ExperimentForm
 
+from quiz.models import Patient
 from quiz.abc_search_engine import Questionnaires
+
+import re
+
 
 @login_required
 def experiment_list(request, template_name="experiment/experiment_list.html"):
-
     experiments = Experiment.objects.order_by('title')
 
     context = {"experiments": experiments}
@@ -21,7 +25,6 @@ def experiment_list(request, template_name="experiment/experiment_list.html"):
 
 @login_required
 def experiment_create(request, template_name="experiment/experiment_register.html"):
-
     experiment_form = ExperimentForm(request.POST or None)
     questionnaires_list = Questionnaires().find_all_active_questionnaires()
 
@@ -63,7 +66,6 @@ def experiment_create(request, template_name="experiment/experiment_register.htm
 
 @login_required
 def experiment_update(request, experiment_id, template_name="experiment/experiment_register.html"):
-
     experiment = get_object_or_404(Experiment, pk=experiment_id)
     questionnaires_list = []
 
@@ -77,7 +79,6 @@ def experiment_update(request, experiment_id, template_name="experiment/experime
             if request.POST['action'] == "save":
 
                 if experiment_form.is_valid():
-
                     experiment_form.save()
 
                     redirect_url = reverse("experiment_edit", args=(experiment_id,))
@@ -91,3 +92,70 @@ def experiment_update(request, experiment_id, template_name="experiment/experime
     return render(request, template_name, context)
 
 
+def subjects(request, experiment_id, template_name="experiment/subjects.html"):
+    experiment = get_object_or_404(Experiment, id=experiment_id)
+    subject_list = experiment.subjects.all()
+
+    context = {
+        'experiment_id': experiment_id,
+        'subject_list': subject_list.all()
+    }
+
+    return render(request, template_name, context)
+
+
+def subjects_insert(request, experiment_id, patient_id):
+    patient = get_object_or_404(Patient, pk=patient_id)
+
+    subject = Subject()
+
+    try:
+        subject = Subject.objects.get(patient=patient)
+    except subject.DoesNotExist:
+        subject.patient = patient
+        subject.save()
+
+    experiment = get_object_or_404(Experiment, id=experiment_id)
+
+    if not experiment.subjects.all().filter(patient=patient):
+        experiment.subjects.add(subject)
+        experiment.save()
+    else:
+        messages.warning(request, 'Participante jah inserido para este experimento.')
+
+    redirect_url = reverse("subjects", args=(experiment_id))
+    return HttpResponseRedirect(redirect_url)  # + "?status=edit&currentTab=3"
+
+
+def subjects_delete(request, experiment_id, subject_id):
+
+    subject = Subject()
+
+    try:
+        subject = Subject.objects.get(pk=subject_id)
+    except subject.DoesNotExist:
+        messages.warning(request, 'Falha ao remover participante do experimento.')
+        redirect_url = reverse("subjects", args=(experiment_id))
+        return HttpResponseRedirect(redirect_url)
+
+    experiment = get_object_or_404(Experiment, id=experiment_id)
+    experiment.subjects.remove(subject)
+
+    messages.warning(request, 'Participante removido do experimento.')
+    redirect_url = reverse("subjects", args=(experiment_id))
+    return HttpResponseRedirect(redirect_url)
+
+
+def search_patients_ajax(request):
+    patient_list = ''
+    if request.method == "POST":
+        search_text = request.POST['search_text']
+        experiment_id = request.POST['experiment_id']
+        if search_text:
+            if re.match('[a-zA-Z ]+', search_text):
+                patient_list = Patient.objects.filter(name_txt__icontains=search_text).exclude(removed=True)
+            else:
+                patient_list = Patient.objects.filter(cpf_id__icontains=search_text).exclude(removed=True)
+
+    return render_to_response('experiment/ajax_search_patients.html',
+                              {'patients': patient_list, 'experiment_id': experiment_id})
