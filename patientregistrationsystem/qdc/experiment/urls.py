15db9ff7cdb685b8e4a58875104f8629c8c95866
Larissa <larissam@ime.usp.br>
2014-09-05 16:04:20 -0300

    Upload consent form (not complete)

--- patientregistrationsystem/qdc/experiment/urls.py
+++ patientregistrationsystem/qdc/experiment/urls.py
@@ -18,7 +18,7 @@ urlpatterns = patterns(
     url(r'^subject/search/$', 'experiment.views.search_patients_ajax', name='subject_search'),
     url(r'^(?P<experiment_id>\d+)/subjects/(?P<patient_id>\d+)/$',
         'experiment.views.subjects_insert', name='subject_insert'),
-    url(r'^(?P<experiment_id>\d+)/subjects/(?P<patient_id>\d+)/upload_file/$',
+    url(r'^(?P<experiment_id>\d+)/subjects/(?P<subject_id>\d+)/upload_file/$',
         'experiment.views.upload_file', name='upload_file'),
 
     # subject + questionnaire
