fd3dcc69f6c5a91b02430d6c65963e26ed5607d4
Larissa <larissam@ime.usp.br>
2014-09-11 11:51:48 -0300

    Bug: error while setting the same questionnaire twice in an experiment

--- patientregistrationsystem/qdc/experiment/views.py
+++ patientregistrationsystem/qdc/experiment/views.py
@@ -115,7 +115,14 @@ def questionnaire_create(request, experiment_id, template_name="experiment/quest
         request.POST or None,
         initial={'number_of_fills': 1, 'interval_between_fills_value': None})
 
-    questionnaires_list = Questionnaires().find_all_active_questionnaires()
+    questionnaires_of_experiment = get_object_or_404(QuestionnaireConfiguration, experiment=experiment)
+    active_questionnaires_list = Questionnaires().find_all_active_questionnaires()
+    for active_questionnaire in active_questionnaires_list:
+        for questionnaire in questionnaires_of_experiment:
+            if active_questionnaire.sid == questionnaire.lime_survey_id:
+                active_questionnaires_list.remove(active_questionnaire)
+                break
+    questionnaires_list = active_questionnaires_list
 
     if request.method == "POST":
 
