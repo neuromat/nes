a98b39230abfe81556dede04a5e8aa78461c8eba
carlos <caduribas@gmail.com>
2014-09-29 14:41:33 -0300

    Committed new files.

---
+++ patientregistrationsystem/qdc/quiz/templatetags/qdc_tags.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+from django import template
+
+register = template.Library()
+
+# settings value
+@register.simple_tag
+def settings_value(name):
+    return getattr(settings, name, "")
