dcc658c7da4bfb0f6c702f626dec707e227ce893
erocha <evandro2001@hotmail.com>
2014-11-13 18:18:37 -0200

    Changed parent attribute in ComponentConfiguration.

--- patientregistrationsystem/qdc/experiment/models.py
+++ patientregistrationsystem/qdc/experiment/models.py
@@ -57,7 +57,7 @@ class Component(models.Model):
     identification = models.CharField(null=False, max_length=50, blank=False)
     description = models.CharField(max_length=150, null=False, blank=False)
     experiment = models.ForeignKey(Experiment, null=False)
-    component_type = models.CharField(null=False, max_length=50,
+    component_type = models.CharField(null=False, max_length=15,
                             choices=(("task", "Task component"),
                                      ("pause", "Pause component"),
                                      ("stimulus", "Stimulus component"),
@@ -77,7 +77,7 @@ class Stimulus(Component):
     stimulus_type = models.CharField(max_length=50, null=False, blank=False)
 
 
-class QuestionnaireComponent(Component):
+class Questionnaire(Component):
     lime_survey_id = models.IntegerField(null=False, blank=False)
 
 
@@ -92,7 +92,7 @@ class ComponentConfiguration(models.Model):
     interval_between_repetitions_value = models.IntegerField(null=True, blank=True, validators=[MinValueValidator(1)])
     interval_between_repetitions_unit = models.ForeignKey(TimeUnit, null=True, blank=True)
     component = models.ForeignKey(Component, null=False, related_name="configuration")
-    parent = models.ForeignKey(Component, null=True, related_name="children")
+    parent = models.ForeignKey('self', null=True, related_name='children')
 
 
 class Group(models.Model):
