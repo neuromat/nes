c3449f45809c93bb072842101e6932c5c7f026a3
larissamoraes <larissamoraes@users.noreply.github.com>
2014-12-16 18:50:08 -0200

    Update admin.py
    Added StimulusType to admin page.

--- patientregistrationsystem/qdc/experiment/admin.py
+++ patientregistrationsystem/qdc/experiment/admin.py
@@ -3,6 +3,7 @@ from experiment.models import TimeUnit, Experiment, QuestionnaireConfiguration, 
 from simple_history.admin import SimpleHistoryAdmin
 
 admin.site.register(TimeUnit)
+admin.site.register(StimulusType)
 admin.site.register(Experiment, SimpleHistoryAdmin)
 admin.site.register(QuestionnaireConfiguration, SimpleHistoryAdmin)
 admin.site.register(QuestionnaireResponse, SimpleHistoryAdmin)
