38b2f0ceb8fa092dfe13163fb0a707f3f39478bd
evandro2001 <evandro2001@hotmail.com>
2015-04-16 15:11:55 -0300

    NES-36: When a questionnaire has more than one language configured, it is necessary to handle them. Otherwise, NES shows the answers for each language..

--- patientregistrationsystem/qdc/experiment/views.py
+++ patientregistrationsystem/qdc/experiment/views.py
@@ -938,7 +938,7 @@ def check_required_fields(surveys, lime_survey_id):
         for group in groups:
             question_list = surveys.list_questions(lime_survey_id, group['id'])
             for question in question_list:
-                question_properties = surveys.get_question_properties(question)
+                question_properties = surveys.get_question_properties(question, None)
                 if question_properties['title'] in fields_to_validate:
                     field = fields_to_validate[question_properties['title']]
                     if not field['found']:
