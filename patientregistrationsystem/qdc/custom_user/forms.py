96db4981ff28b85196482aa3bb775024e1f0302a
diogopedrosa <diogocpedrosa@gmail.com>
2015-06-01 09:20:08 -0300

    Bug fix: It was not possible to change the username of a user because the system was complaining saying that the email already exists.

--- patientregistrationsystem/qdc/custom_user/forms.py
+++ patientregistrationsystem/qdc/custom_user/forms.py
@@ -14,7 +14,8 @@ class UserForm(ModelForm):
             'first_name': TextInput(attrs={'class': 'form-control', 'placeholder': 'Entrar primeiro nome'}),
             'last_name': TextInput(attrs={'class': 'form-control', 'placeholder': 'Entrar último nome'}),
             'username': TextInput(attrs={'class': 'form-control', 'placeholder': 'Entrar nome de usuário'}),
-            'password': PasswordInput(attrs={'id': 'id_new_password1', 'class': 'form-control', 'placeholder': 'Entrar senha',
+            'password': PasswordInput(attrs={'id': 'id_new_password1', 'class': 'form-control',
+                                             'placeholder': 'Entrar senha',
                                              'onkeyup': "passwordForce(); if(beginCheckPassword1)checkPass();"}),
             'email': TextInput(attrs={'class': 'form-control', 'placeholder': 'Entrar e-mail', 'id': "email",
                                       'type': 'email', 'data-error': "E-mail inválido",
@@ -28,16 +29,17 @@ class UserForm(ModelForm):
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
-        username = self.cleaned_data.get('username')
-        if email and User.objects.filter(email=email, is_active=True).exclude(username=username).count():
+        if email and User.objects.filter(email=email, is_active=True).exclude(id=self.instance.id).count():
             raise ValidationError(u'Este email já existe.')
         return email
 
 
 class UserFormUpdate(UserForm):
     password = CharField(required=False,
-                         widget=PasswordInput(attrs={'id': 'id_new_password1', 'class': 'form-control', 'placeholder': 'Entrar senha',
-                                                     'onkeyup': "passwordForce(); if(beginCheckPassword1)checkPass();"}))
+                         widget=PasswordInput(attrs={'id': 'id_new_password1', 'class': 'form-control',
+                                                     'placeholder': 'Entrar senha',
+                                                     'onkeyup': "passwordForce(); "
+                                                                "if(beginCheckPassword1) checkPass();"}))
 
     def clean_password(self):
         if self.cleaned_data['password']:
