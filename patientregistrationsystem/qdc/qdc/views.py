2a5b92ea88eef13703eed92b2187f3e15ce0cac0
diogopedrosa <diogocpedrosa@gmail.com>
2015-06-30 09:04:32 -0300

    NES-30 Parameterize contact information that are shown in the home page NES-106 Check interfaces in smaller screens (tablet and phone) - Make footer appears bellow content, not side by side NES-104 Create a python script to populate DB with initial data and upload sql script to create SID tables - Python script created. Only sql script missing.

--- patientregistrationsystem/qdc/qdc/views.py
+++ patientregistrationsystem/qdc/qdc/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
-
+from django.shortcuts import render
 
 def qdc_permission_denied_view(request, template_name="admin/qdc_403.html"):
 
@@ -11,4 +11,10 @@ def qdc_permission_denied_view(request, template_name="admin/qdc_403.html"):
 
 @login_required
 def contact(request):
-    return render(request, 'quiz/contato.html')
+    context = {
+        'contact_email': settings.CONTACT_EMAIL,
+        'contact_institution': settings.CONTACT_INSTITUTION,
+        'contact_url': settings.CONTACT_URL,
+    }
+
+    return render(request, 'quiz/contato.html', context)
