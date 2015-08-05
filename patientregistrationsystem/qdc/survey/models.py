c2a809634ec7deb15e23deab97b967aae4595abe
diogopedrosa <diogocpedrosa@gmail.com>
2015-08-05 15:58:13 -0300

    •	Improvement: New survey has the value True as default for the field boolean is_initial_evaluation, because this functionality is useful only for creating new initial evaluations.

--- patientregistrationsystem/qdc/survey/models.py
+++ patientregistrationsystem/qdc/survey/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Survey(models.Model):
     lime_survey_id = models.IntegerField(null=False, blank=False, unique=True)
-    is_initial_evaluation = models.BooleanField(null=False, blank=False, default=False)
+    is_initial_evaluation = models.BooleanField(null=False, blank=False, default=True)
 
     class Meta:
         permissions = (
