f917a57ceaabc79725f18a38f1d3577df6c9bc11
evandro2001 <evandro2001@hotmail.com>
2016-03-29 19:08:05 -0300

    NES-240 - In the subject of group screen, manage the EEG data collection from the subjects

--- patientregistrationsystem/qdc/experiment/translation.py
+++ patientregistrationsystem/qdc/experiment/translation.py
@@ -1,8 +1,14 @@
 from modeltranslation.translator import translator, TranslationOptions
-from experiment.models import StimulusType
+from experiment.models import StimulusType, FileFormat
 
 
 class StimulusTypeTranslationOptions(TranslationOptions):
     fields = ('name', )
 
 translator.register(StimulusType, StimulusTypeTranslationOptions)
+
+
+class FileFormatTranslationOptions(TranslationOptions):
+    fields = ('name', 'description', )
+
+translator.register(FileFormat, FileFormatTranslationOptions)
