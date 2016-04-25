79173ef1e6c13744b9557f605275e1ec91f561dc
carlosribas <caduribas@gmail.com>
2016-04-25 18:32:13 -0300

    NES-258 - Allow the user to choose a file format "other", describing about its format

--- patientregistrationsystem/qdc/quiz/static/quiz/js/eeg_data_collection.js
+++ patientregistrationsystem/qdc/quiz/static/quiz/js/eeg_data_collection.js
@@ -2,12 +2,8 @@ $(document).ready(function () {
 
 	var $id_file_format = $("#id_file_format");
 
-	$id_file_format.each(function() {
-		if ($("#id_file_format option:selected").text() == "Other")
-            $id_file_format.parents('.row').next().show();
-		else
-			$id_file_format.parents('.row').next().hide();
-	});
+    // ### Hide file format description field ###
+    $id_file_format.parents('.row').next().hide();
 
     $id_file_format.on('change', (function() {
 		if ($("#id_file_format option:selected").text() == "Other")
