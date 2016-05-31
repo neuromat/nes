956aef3e591d005b939f79ada31fb5eb93ffddf1
evandro2001 <evandro2001@hotmail.com>
2016-05-31 15:00:25 -0300

    NES-355 - Create a screen to add a EEG Electrode Layout setting to a EEG Setting

--- patientregistrationsystem/qdc/experiment/admin.py
+++ patientregistrationsystem/qdc/experiment/admin.py
@@ -3,7 +3,9 @@ from simple_history.admin import SimpleHistoryAdmin
 from modeltranslation.admin import TranslationAdmin
 
 from experiment.models import Experiment, QuestionnaireResponse, StimulusType, FileFormat, \
-    Manufacturer, Equipment, EEGElectrodeLocalizationSystem
+    Manufacturer, Equipment, EEGElectrodeLocalizationSystem, EEGMachine, EEGFilterType, EEGAmplifier, EEGSolution, \
+    EEGElectrodeNet, EEGElectrodeCap, EEGElectrodePosition, EEGElectrodeNetSystem, EEGCapSize, EEGElectrodeModel
+
 
 admin.site.register(Experiment, SimpleHistoryAdmin)
 admin.site.register(QuestionnaireResponse, SimpleHistoryAdmin)
@@ -22,4 +24,14 @@ admin.site.register(FileFormat, FileFormatAdmin)
 admin.site.register(Manufacturer)
 admin.site.register(Equipment)
 
+admin.site.register(EEGMachine)
+admin.site.register(EEGFilterType)
+admin.site.register(EEGAmplifier)
+admin.site.register(EEGSolution)
+admin.site.register(EEGElectrodeNet)
+admin.site.register(EEGElectrodeCap)
 admin.site.register(EEGElectrodeLocalizationSystem)
+admin.site.register(EEGElectrodePosition)
+admin.site.register(EEGElectrodeNetSystem)
+admin.site.register(EEGCapSize)
+admin.site.register(EEGElectrodeModel)
