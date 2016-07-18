c579ca6896c64c087ac6f9a454c241febaa2e34e
evandro2001 <evandro2001@hotmail.com>
2016-07-18 17:58:30 -0300

    NES-428 - Filter Muscle Side when a Placement is selected

--- patientregistrationsystem/qdc/quiz/static/quiz/js/emg_electrode_setting_register.js
+++ patientregistrationsystem/qdc/quiz/static/quiz/js/emg_electrode_setting_register.js
@@ -11,3 +11,32 @@ function show_modal_remove_setting(emg_electrode_setting_type){
     modal_remove.setAttribute("value", 'remove-' + emg_electrode_setting_type);
     $('#modalRemoveElectrodeSetting').modal('show');
 }
+
+$(document).ready(function () {
+    
+    var emg_electrode_placement = $("#id_emg_electrode_placement");
+    
+    emg_electrode_placement.change(function() {
+
+        var emg_electrode_placement_id = $(this).val();
+        var select_muscle_side = $("select#id_muscle_side");
+
+        if (emg_electrode_placement_id == "") {
+            emg_electrode_placement_id = "0";
+        }
+
+        var url = "/experiment/emg_setting/get_muscle_side_by_electrode_placement/" + emg_electrode_placement_id;
+
+        $.getJSON(url, function(all_sides) {
+            var options = '<option value="" selected="selected">---------</option>';
+            for (var i = 0; i < all_sides.length; i++) {
+                options += '<option value="' + all_sides[i].pk + '">' + all_sides[i].fields['name'] + '</option>';
+            }
+            select_muscle_side.html(options);
+            // select_equipment.change();
+        });
+
+
+    });
+
+});
