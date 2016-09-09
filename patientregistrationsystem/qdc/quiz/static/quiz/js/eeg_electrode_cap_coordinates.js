a9b3cbc178dc47f76668f1cf7a740bfa1c95790e
mruizolazar <margaritaruizolazar@gmail.com>
2016-09-09 12:21:44 -0300

    NES-504 - Electrode localization system. Error when insert using image.

--- patientregistrationsystem/qdc/quiz/static/quiz/js/eeg_electrode_cap_coordinates.js
+++ patientregistrationsystem/qdc/quiz/static/quiz/js/eeg_electrode_cap_coordinates.js
@@ -11,6 +11,7 @@ var can_update = false;
 var point_update = 0;
 var point_update_name = "";
 
+// função que inicializa as variaveis da página
 function init(){
     var canvas = document.getElementById("electrodeMapCanvas");
     var ctx = canvas.getContext("2d");
@@ -32,6 +33,7 @@ function init(){
     canvas.addEventListener("mousedown", getPosition, false);
 }
 
+// função utilizada para ordenar dois posições
 function compare(a,b) {
   if (a.position < b.position)
     return -1;
@@ -42,7 +44,7 @@ function compare(a,b) {
 
 //objs.sort(compare);
 
-//cria dinamicamente a tabela e os checkboxs
+//cria dinamicamente a lista de posições e os buttons para remover
 function  addSetted(){
     positions.sort(compare);
     for(var i in positions){
@@ -108,6 +110,7 @@ function  addSetted(){
     }
 }
 
+// função chamada ao fazer click sobre o nome da posição da lista de posições para realizar sua atualização
 function update(positionId) {
     point_update = positionId;
     if (can_update) refresh_Screen();
@@ -142,7 +145,7 @@ function update(positionId) {
     }
 };
 
-//Funcao que coloca delete = true para posteriormente ser deletado do banco de dados
+//Funcao que coloca delete = true para posteriormente ser apagado do banco de dados
 function setted(positionId){
     var canvas = document.getElementById("electrodeMapCanvas");
     var ctx = canvas.getContext("2d");
@@ -159,6 +162,7 @@ function setted(positionId){
 
 };
 
+// função que desenha um circulo na imagem para indicar uma posição
 function pintar(){
     var canvas = document.getElementById("electrodeMapCanvas");
     var context = canvas.getContext("2d");
@@ -188,7 +192,7 @@ function pintar(){
     }
 }; //fim function pintar
 
-
+// função que insere um novo valor na lista de posições
 function addRow(index){
     var position = positions[index-1];
     id = position.id;
@@ -242,6 +246,7 @@ function addRow(index){
     posTable.appendChild(row);
 }
 
+// função que recarrega a imagem no canvas
 function refresh_Screen(){
     var canvas = document.getElementById("electrodeMapCanvas");
     var ctx = canvas.getContext("2d");
@@ -257,6 +262,7 @@ function refresh_Screen(){
     imageObj.src = map_file.value;
 };
 
+// função que valida se o mouse foi clickado numa posição que já existe
 function validPosition(new_x, new_y){
     var x = parseInt(new_x);
     var y = parseInt(new_y);
@@ -265,28 +271,47 @@ function validPosition(new_x, new_y){
 
     for(var i in positions) {
         position = positions[i];
-        if(!position.delete){
+        //if(!position.delete){
             dx = x - parseInt(position.x);
             dy = y - parseInt(position.y);
 
             dist = Math.sqrt(dx*dx + dy*dy);
             if(dist < radio){
                 valid= true;
-                if(position.used){
-                    alert(gettext("This coodinates can't be modified because is being used by some EEG layout setting! "));
-                }else{
-                    btn = document.getElementById(position.id);
-                    position.delete = true;
-                    var i = btn.parentNode.parentNode.rowIndex;
-                    document.getElementById("cap_positions").deleteRow(i);
+                if(!position.delete){
+                    if(position.used){
+                        alert(gettext("This coodinates can't be modified because is being used by some EEG layout setting! "));
+                    }else{
+                        btn = document.getElementById(position.id);
+                        position.delete = true;
+                        var i = btn.parentNode.parentNode.rowIndex;
+                        document.getElementById("cap_positions").deleteRow(i);
+                    }
+                }else {
+                    alert(gettext("Updating point ") + position.position);
+                    if (confirm(gettext("Confirms the coordinates? x - y: ") + x + " - " + y) == true) {
+                        var name = prompt(gettext("Please enter the name of the point"), position.position);
+                        if(name != null){
+                            // chkbox = document.getElementById(position.id);
+                            // chkbox.checked = true;
+                            position.position = name,
+                            position.x = x,
+                            position.y = y,
+                            position.update = true,
+                            position.delete = false
+                        }
+                    }
+                    //refresh_Screen();
+                    index = parseInt(i)+1;
+                    addRow(index);
                 }
             }
-        }
-
+        //}
     }
     return valid;
 }
 
+// função que captura o evento 'click do mouse
 function getPosition(event){
     var x = new Number();
     var y = new Number();
@@ -295,56 +320,15 @@ function getPosition(event){
 
     var rect = this.getBoundingClientRect();
 
-    // if (event.layerX || event.layerX == 0) { // Firefox
-    //     event._x = event.layerX;
-    //     event._y = event.layerY;
-    // } else if (event.offsetX || event.offsetX == 0) { // Opera
-    //     event._x = event.offsetX;
-    //     event._y = event.offsetY;
-    // }
-
     var coords = {
         x: event.clientX - rect.left,
         y: event.clientY - rect.top
     };
 
-    // x = parseInt(event._x);
-    // y = parseInt(event._y);
-
     x = parseInt(coords.x);
     y = parseInt(coords.y);
 
-    if(!can_update){
-        if(validPosition(x,y)) refresh_Screen();
-        else{
-             if (confirm(gettext("Confirms the coordinates? x - y: ") + x + " - " + y) == true) {
-                 var id = positions.length + 1;
-                 var name = prompt(gettext("Please enter the name this point"), id);
-                 if(name != null){
-                     id = localization_system_id + "_" + id;
-                     context.beginPath();
-                     context.arc(x, y, 5, 0, 2 * Math.PI);
-                     context.fillStyle = "red";
-                     context.fill();
-                     context.stroke();
-                     positions.push({
-                         id : id,
-                         position: name,
-                         x: x,
-                         y: y,
-                         used: false, //this point is not used by some layout
-                         existInDB: false, //this point doesn't exist in the DB
-                         delete: false, //this point was not deleted
-                         update: false  //this point is not updated
-                     });
-                     used_positions_counter++;
-                     var index = new Number();
-                     index = positions.length;
-                     addRow(index);
-                 }
-             }
-        }
-    }else{
+    if(can_update){
         alert(gettext("Updating point ") + point_update_name);
         can_update = false;
         if (confirm(gettext("Confirms the coordinates? x - y: ") + x + " - " + y) == true) {
@@ -353,21 +337,52 @@ function getPosition(event){
                 for(var i in positions){
                     var position = positions[i];
                     if(position.id == point_update){
-                        chkbox = document.getElementById(position.id);
-                        chkbox.checked = true;
-                        position.name = name,
+                        position.position = name,
                         position.x = x,
-                        position.y = y,
-                        position.update = true
+                        position.y = y
+                        a =  document.getElementsByName(position.id);
+                        a.innerHTML = name;
                     }
                 }
             }
         }
+        //addSetted();
         refresh_Screen();
+    }else if(validPosition(x,y) && !can_update) {
+        refresh_Screen();
+    }else{
+        if(!can_update) {
+            if (confirm(gettext("Confirms the coordinates? x - y: ") + x + " - " + y) == true) {
+                var id = positions.length + 1;
+                var name = prompt(gettext("Please enter the name this point"), id);
+                if (name != null) {
+                    id = localization_system_id + "_" + id;
+                    context.beginPath();
+                    context.arc(x, y, 5, 0, 2 * Math.PI);
+                    context.fillStyle = "red";
+                    context.fill();
+                    context.stroke();
+                    positions.push({
+                        id: id,
+                        position: name,
+                        x: x,
+                        y: y,
+                        used: false, //this point is not used by some layout
+                        existInDB: false, //this point doesn't exist in the DB
+                        delete: false, //this point was not deleted
+                        update: false  //this point is not updated
+                    });
+                    used_positions_counter++;
+                    var index = new Number();
+                    index = positions.length;
+                    addRow(index);
+                }
+            }
+        }
     }
-
 };
 
+// função que envia a lista de posições ao servidor para ser atualizadas no banco de dados
 function sendPositions(){
     var url = "/experiment/eeg_electrode_localization_system/get_positions/" + localization_system_id;
 
