030a8adc79682266e47e876068bf30470e8bb5ca
mruizolazar <margaritaruizolazar@gmail.com>
2016-11-10 19:44:11 -0200

    NES-549 - Allow to register pulse placement, hotspot and rest motor threshold (RMT) for TMS steps (for participant)

--- patientregistrationsystem/qdc/experiment/views.py
+++ patientregistrationsystem/qdc/experiment/views.py
@@ -589,7 +589,7 @@ def group_view(request, group_id, template_name="experiment/group_register.html"
         experimental_protocol_description = get_experimental_protocol_description(group.experimental_protocol,
                                                                                   request.LANGUAGE_CODE)
 
-        # experimental_protocol_picture = get_experimental_protocol_picture(group.experimental_protocol, request.LANGUAGE_CODE)
+        experimental_protocol_picture = get_experimental_protocol_picture(group.experimental_protocol, request.LANGUAGE_CODE)
 
     context = {"can_change": can_change,
                "classification_of_diseases_list": group.classification_of_diseases.all(),
