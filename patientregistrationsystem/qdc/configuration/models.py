4276e208681a8add6689bcdffc0c2cb2f0a099f4
evandro2001 <evandro2001@hotmail.com>
2017-02-09 20:55:57 -0200

    NES-591 Register information about the NES installation in order to identify data origin when integrate with Portal

---
+++ patientregistrationsystem/qdc/configuration/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from solo.models import SingletonModel
+
+
+def get_institution_logo_dir(instance, filename):
+    return "institution_logo/%s/%s" % (instance.id, filename)
+
+
+class Institution(SingletonModel):
+    name = models.CharField(max_length=255, null=True, blank=True)
+    url = models.URLField(null=True, blank=True)
+    logo = models.FileField(upload_to=get_institution_logo_dir, null=True, blank=True)
+
+
+class Contact(SingletonModel):
+    name = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
