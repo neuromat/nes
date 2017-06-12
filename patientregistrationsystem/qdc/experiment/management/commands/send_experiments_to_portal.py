a8c5cb0625afcad830b94e5df623a102550b43c3
evandro2001 <evandro2001@hotmail.com>
2017-06-12 17:35:15 -0300

    NES-659 Send experiment through command line, as cron

---
+++ patientregistrationsystem/qdc/experiment/management/commands/send_experiments_to_portal.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from experiment.views import send_all_experiments_to_portal
+
+
+class Command(BaseCommand):
+    help = 'Send all experiments that were scheduled to portal'
+
+    def handle(self, *args, **options):
+        print('sending...')
+        send_all_experiments_to_portal(settings.LANGUAGE_CODE)
+        print('sent')
