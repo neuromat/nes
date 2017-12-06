36cd2dbd4886849b1b26968ed2ec32412cda0c4d
mruizolazar <margaritaruizolazar@gmail.com>
2017-12-06 15:40:22 -0200

    NES-732 Adjust the NES interface to facilitate the management of EEG data  and metadata (From Neu...

--- patientregistrationsystem/qdc/experiment/migrations/0002_load_BrainVision_FileFormat.py
+++ patientregistrationsystem/qdc/experiment/migrations/0002_load_BrainVision_FileFormat.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import migrations, models
+from django.db import migrations
+
 
 def backwards_data(apps, schema_editor):
     pass
@@ -18,15 +19,6 @@ def load_data(apps, schema_editor):
             description="File format to stores the three different filetypes (.vhdr, .eeg, .vmrk) from Brain Vision "
                         "Recorder Software",
             tags="EEG",
-        ).save()
-
-    if not file_format_model.objects.filter(nes_code="other"):
-        file_format_model(
-            nes_code="other",
-            extesion=".*",
-            description="File format for generic filetypes",
-            name="Other",
-            tags="",
         ).save()
 
 
