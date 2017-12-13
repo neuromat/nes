edfa506693e90ba90afaaf08060f52495ba74388
mruizolazar <margaritaruizolazar@gmail.com>
2017-12-13 11:54:27 -0200

    NES-732 Adjust the NES interface to facilitate the management of EEG data  and metadata (From Neu...

--- patientregistrationsystem/qdc/experiment/migrations/0002_load_BrainVision_FileFormat.py
+++ patientregistrationsystem/qdc/experiment/migrations/0002_load_BrainVision_FileFormat.py
@@ -10,6 +10,7 @@ def backwards_data(apps, schema_editor):
 
 def load_data(apps, schema_editor):
     file_format_model = apps.get_model("experiment", "FileFormat")
+    tag_model = apps.get_model("experiment", "Tag")
 
     if not file_format_model.objects.filter(nes_code="MNE-RawFromBrainVision"):
         file_format_model(
@@ -18,8 +19,17 @@ def load_data(apps, schema_editor):
             extension=".eeg .vhdr .vmrk",
             description="File format to stores the three different filetypes (.vhdr, .eeg, .vmrk) from Brain Vision "
                         "Recorder Software",
-            tags="EEG",
+            name_en="EEG raw data - Brain Vision binary (MNE)",
+            name_pt_br="Dados brutos de EEG - Brain Vision (MNE)",
         ).save()
+
+    file_format_mne = file_format_model.objects.get(nes_code="MNE-RawFromBrainVision")
+
+    # tag for EEG
+    for tag in tag_model.objects.all():
+        if tag.name == "EEG":
+            file_format_mne.tags.add(tag)
+            file_format_mne.save()
 
 
 class Migration(migrations.Migration):
