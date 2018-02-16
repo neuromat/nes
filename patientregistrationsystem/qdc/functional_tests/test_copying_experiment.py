7abfb1cdba8d70a0e27f1651256fc2c72858d06b
caco <crns13@gmail.com>
2018-02-16 10:04:28 -0200

    First functional test passing: test login - this is an intermediary level test done to test what the issue really asks for

---
+++ patientregistrationsystem/qdc/functional_tests/test_copying_experiment.py
@@ -0,0 +1,35 @@
+from django.conf import settings
+from django.contrib.auth import SESSION_KEY, BACKEND_SESSION_KEY
+from django.contrib.sessions.backends.db import SessionStore
+
+from custom_user.tests_helper import create_user
+from functional_tests.base import FunctionalTest
+from functional_tests.test_login import TEST_USERNAME
+
+
+class CopyExperimentTest(FunctionalTest):
+
+    def create_pre_authenticated_session(self):
+        user = create_user(
+            group_name='Senior researcher', username=TEST_USERNAME
+        )
+        session = SessionStore()
+        session[SESSION_KEY] = user.pk
+        session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
+        session.save()
+        # to set a cookie we need to first visit the domain.
+        # 404 pages load the quickest
+        self.browser.get(self.live_server_url + '/404_no_such_url')
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session.session_key,
+            path='/'
+        ))
+
+    def test_can_see_copy_experiment_button(self):
+        # Suzana, a senior researcher categorized in NES, is trying to change
+        # an experiment with an already defined experimental protocol. The
+        # system does not allow that, so a modal appears telling her that
+        # she can copy the experiment with all experiment data.
+
+        self.create_pre_authenticated_session()
