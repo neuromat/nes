21eeb0c29db78eb90d9f40f18e11093d364ac3ac
caco <crns13@gmail.com>
2018-04-17 10:35:52 -0300

    NES-808 - In Export, for multiple choice question, put unselected options with "N"

--- patientregistrationsystem/qdc/survey/urls.py
+++ patientregistrationsystem/qdc/survey/urls.py
@@ -2,12 +2,11 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     'survey.views',
-
     url(r'^list/$', 'survey_list', name='survey_list'),
     url(r'^(?P<survey_id>\d+)/$', 'survey_view', name='survey_view'),
     url(r'^new/$', 'survey_create', name='survey_create'),
     url(r'^edit/(?P<survey_id>\d+)/$', 'survey_update', name='survey_edit'),
     url(r'^edit/(?P<survey_id>\d+)/sensitive_questions/$',
-        'survey_update_sensitive_questions', name='survey_edit_sensitive_questions'),
-
+        'survey_update_sensitive_questions',
+        name='survey_edit_sensitive_questions'),
 )
