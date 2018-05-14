b330861f0f6c409ec1f2ab3726995e9b44c2ae39
Celso Oviedo da S Lopes <celsovi@Sala20-02-i9.local>
2018-05-14 11:53:34 -0300

    Commiting same wsgi_default.py

--- patientregistrationsystem/qdc/qdc/wsgi_default.py
+++ patientregistrationsystem/qdc/qdc/wsgi_default.py
@@ -1,8 +1,6 @@
 """
 WSGI config for qdc project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
