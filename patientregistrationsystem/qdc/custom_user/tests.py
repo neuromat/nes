2e257b45d069e4319e29dab2ed90d0ff1f954836
carlosribas <caduribas@gmail.com>
2018-09-05 14:53:44 -0300

    NES-873 - Improve test coverage for the custom_user app

--- patientregistrationsystem/qdc/custom_user/tests.py
+++ patientregistrationsystem/qdc/custom_user/tests.py
@@ -3,6 +3,7 @@ import re
 
 from django.contrib.auth.models import Group
 from django.contrib.auth.tokens import default_token_generator
+from django.contrib.messages import get_messages
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404
@@ -10,15 +11,17 @@ from django.template import loader
 from django.test import TestCase
 from django.test.client import RequestFactory
 from django.utils.http import int_to_base36
+from django.utils.translation import ugettext as _
 
 from custom_user.models import User, UserProfile
 from custom_user.views import user_update
 
-# Constantes para testes de User
-USER_EDIT = 'user_edit'
 USER_USERNAME = 'myadmin'
 USER_PWD = 'mypassword'
 USER_NEW = 'user_new'
+USER_VIEW = 'user_view'
+USER_EDIT = 'user_edit'
+PATTERN = '((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})'
 
 
 class FormUserValidation(TestCase):
@@ -26,8 +29,6 @@ class FormUserValidation(TestCase):
     user = ''
 
     def setUp(self):
-        """ Configura autenticacao e variaveis para iniciar cada teste """
-
         self.user = User.objects.create_superuser(username=USER_USERNAME, email='jenkins.neuromat@gmail.com',
                                                   password=USER_PWD)
         self.user.is_staff = True
@@ -97,32 +98,7 @@ class FormUserValidation(TestCase):
         msg.attach_alternative(html_content, "text/html")
         msg.send()
 
-    def test_user_password_pattern(self):
-        """
-        Testa o pattern definido
-
-        Detalhamento do pattern
-        (			# Start of group
-        (?=.*\d)		#   must contains one digit from 0-9
-        (?=.*[a-z])		#   must contains one lowercase characters
-        (?=.*[A-Z])		#   must contains one uppercase characters
-        (?=.*[@#$%])		#   must contains one special symbols in the list "@#$%"
-        .		#     match anything with previous condition checking
-        {6,20}	#        length at least 6 characters and maximum of 20
-        )
-        """
-        pattern = '((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})'
-        password = "abcC2@!$"
-
-        self.assertTrue(self.confirm_password(pattern, password), True)
-
-    @staticmethod
-    def confirm_password(pattern, password):
-        return re.compile(pattern).match(password)
-
     def test_user_invalid_username(self):
-        """ Teste de inclusao de usuario com nome de usuario invalido"""
-
         self.data['username'] = ''
         self.data['login_enabled'] = True
 
@@ -132,10 +108,6 @@ class FormUserValidation(TestCase):
         self.assertEqual(User.objects.filter(username='').count(), 0)
 
     def test_user_invalid_email(self):
-        """
-        Testa inclusao de usuario com sucesso
-        """
-
         self.data['email'] = 'email@invalid.'
         self.data['login_enabled'] = True
 
@@ -143,30 +115,25 @@ class FormUserValidation(TestCase):
         self.assertFormError(response, "form", "email", 'Informe um endereço de email válido.')
         self.assertEqual(User.objects.filter(username='').count(), 0)
 
-    def test_user_duplicated_email(self):
-        """
-        Testa inclusao de usuario com sucesso
-        """
-        username = 'test_username'
-        self.data['username'] = username
-        self.data['login_enabled'] = True
-
-        response = self.client.post(reverse(USER_NEW), self.data)
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(User.objects.filter(username=username).count(), 1)
-
-        username2 = 'test_username2'
-        self.data['username']=[username2]
-        self.data['first_name'] = ['Fulano']
+    def test_user_email_already_registered(self):
+        self.data['username'] = 'email_registered'
+        self.data['first_name'] = 'Fulano'
+        self.data['last_name'] = 'de Tal'
+        self.data['email'] = 'jenkins.neuromat@gmail.com'
+        self.data['login_enabled'] = False
 
         response = self.client.post(reverse(USER_NEW), self.data, follow=True)
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]),
+                         _('A researcher with this email address has already been registered before. '
+                           'Please contact your system administrator if you want to reactivate this account.')
+                         )
+        self.assertEqual(User.objects.filter(username='email_registered').count(), 0)
 
         self.assertEqual(response.status_code, 200)
 
     def test_researcher_without_user(self):
-        """
-        Testa inclusao de usuario com sucesso
-        """
         del self.user
         self.data['login_enabled'] = False
 
@@ -174,9 +141,6 @@ class FormUserValidation(TestCase):
         self.assertEqual(response.status_code, 302)
 
     def test_user_passwords_doesnt_match(self):
-        """
-        Testa senhas não conferem
-        """
         user_pwd = 'test_pwd'
         self.data['username'] = user_pwd
         self.data['password'] = 'abc123'
@@ -186,50 +150,7 @@ class FormUserValidation(TestCase):
         self.client.post(reverse(USER_NEW), self.data, follow=True)
         self.assertEqual(User.objects.filter(username=user_pwd).count(), 1)
 
-    def test_user_password_check_valid_pattern(self):
-        """
-        Testa padrao valido de senha definido para o usuario
-        """
-        user_pwd = 'test_pwd_1'
-        pattern = '((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})'
-        password = 'Abc$123'
-        self.assertTrue(self.confirm_password(pattern=pattern, password=password), True)
-
-        self.data['username'] = user_pwd
-        self.data['password'] = password
-        self.data['login_enabled'] = True
-
-        self.client.post(reverse(USER_NEW), self.data, follow=True)
-        self.assertEqual(User.objects.filter(username=user_pwd).count(), 1)
-
-    def test_user_password_check_invalid_pattern_abc(self):
-        """
-        Testa padrao invalido de senha definido para o usuario (senha: abc)
-        """
-        user_pwd = 'test_pwd_1'
-        self.data['username'] = user_pwd
-        self.data['password'] = 'abc'
-        self.data['login_enabled'] = True
-
-        self.client.post(reverse(USER_NEW), self.data, follow=True)
-        self.assertEqual(User.objects.filter(username=user_pwd).count(), 1)
-
-    def test_user_password_check_invalid_pattern_123(self):
-        """
-        Testa padrao invalido de senha definido para o usuario (senha: 123)
-        """
-        user_pwd = 'test_pwd_1'
-        self.data['username'] = user_pwd
-        self.data['password'] = '123'
-        self.data['login_enabled'] = True
-
-        self.client.post(reverse(USER_NEW), self.data, follow=True)
-        self.assertEqual(User.objects.filter(username=user_pwd).count(), 1)
-
     def test_user_empty_password(self):
-        """
-        Testa senha em branco
-        """
         user_pwd = 'test_pwd_2'
         self.data['username'] = user_pwd
         self.data['password'] = ''
@@ -241,9 +162,6 @@ class FormUserValidation(TestCase):
         self.assertEqual(User.objects.filter(username=user_pwd).count(), 0)
 
     def test_user_create(self):
-        """
-        Testa inclusao de usuario com sucesso
-        """
         username = 'test_username'
         self.data['username'] = username
         self.data['login_enabled'] = True
@@ -253,9 +171,6 @@ class FormUserValidation(TestCase):
         self.assertEqual(User.objects.filter(username=username).count(), 1)
 
     def test_user_create_mail_password_define(self):
-        """
-        Testa inclusao de usuario com sucesso
-        """
         username = 'test_username'
         self.data['email'] = 'romulojosefranco@gmail.com'
         self.data['username'] = username
@@ -273,10 +188,6 @@ class FormUserValidation(TestCase):
         self.reset(user_added, request)
 
     def test_user_read(self):
-        """
-        Testa visualizar usuario
-        """
-
         self.data['login_enabled'] = True
         # Create an instance of a GET request.
         request = self.factory.get(reverse(USER_EDIT, args=[self.user.pk]))
@@ -289,10 +200,6 @@ class FormUserValidation(TestCase):
         self.assertEqual(response.status_code, 302)
 
     def test_user_update_get(self):
-        """
-        Testa atualizar usuario - Metodo GET
-        """
-
         # Create an instance of a GET request.
         request = self.factory.get(reverse(USER_EDIT, args=[self.user.pk]))
         request.user = self.user
@@ -302,10 +209,6 @@ class FormUserValidation(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_user_update_post(self):
-        """
-        Testa atualizar usuario - Metodo POST
-        """
-
         first_name = 'test_username'
         self.data['first_name'] = first_name
         self.data['login_enabled'] = True
@@ -319,9 +222,6 @@ class FormUserValidation(TestCase):
         self.assertEqual(user_first_name.first_name, first_name)
 
     def test_user_remove(self):
-        """
-        Testa deletar usuario
-        """
         user_str = 'user_remove'
         user_to_delete = User.objects.create_user(username=user_str, email='test@delete.com',
                                                   password='Del!123')
@@ -345,3 +245,86 @@ class FormUserValidation(TestCase):
 
         response = user_update(request, user_id=user_to_delete.pk)
         self.assertEqual(response, None)
+
+    def test_create_researcher_without_system_access(self):
+        username = 'fulano@detal.com'
+        self.data['first_name'] = 'Fulano'
+        self.data['last_name'] = 'de Tal'
+        self.data['email'] = 'fulano@detal.com'
+        self.data['username'] = username
+        self.data['login_enabled'] = False
+        self.client.post(reverse(USER_NEW), self.data)
+
+        fulano = User.objects.get(username=username)
+        self.assertTrue(fulano.is_active)
+        self.assertFalse(fulano.has_usable_password())
+
+    def test_remove_researcher_without_system_access(self):
+        username = 'fulano@detal.com'
+        self.data['first_name'] = 'Fulano'
+        self.data['last_name'] = 'de Tal'
+        self.data['email'] = 'fulano@detal.com'
+        self.data['username'] = username
+        self.data['login_enabled'] = False
+        self.client.post(reverse(USER_NEW), self.data)
+
+        resercher = User.objects.get(username=username)
+        self.data['action'] = 'remove'
+        response = self.client.post(reverse(USER_VIEW, args=(resercher.pk,)), self.data, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+
+class PasswordPattern(TestCase):
+    """
+        Test of the defined pattern
+
+        (	    		#   Start
+        (?=.*\d)		#   must contains one digit from 0-9
+        (?=.*[a-z])		#   must contains one lowercase characters
+        (?=.*[A-Z])		#   must contains one uppercase characters
+        (?=.*[@#$%])	#   must contains one special symbols in the list "@#$%"
+        .		        #   match anything with previous condition checking
+        {6,20}	        #   length at least 6 characters and maximum of 20
+        )               #   End
+    """
+
+    @staticmethod
+    def confirm_password(pattern, password):
+        return re.compile(pattern).match(password)
+
+    def test_user_password_check_valid_pattern(self):
+        password = "abcC2@!$"
+
+        self.assertTrue(self.confirm_password(PATTERN, password), True)
+
+    def test_user_password_check_invalid_pattern_only_letters(self):
+        password = 'abcdefgh'
+
+        self.assertFalse(self.confirm_password(pattern=PATTERN, password=password))
+
+    def test_user_password_check_invalid_pattern_only_numbers(self):
+        password = '12345678'
+
+        self.assertFalse(self.confirm_password(pattern=PATTERN, password=password))
+
+    def test_user_password_check_invalid_pattern_without_special_symbols(self):
+        password = '123abcDEF'
+
+        self.assertFalse(self.confirm_password(pattern=PATTERN, password=password))
+
+    def test_user_password_check_invalid_pattern_less_than_six_chars(self):
+        password = '1aB!'
+
+        self.assertFalse(self.confirm_password(pattern=PATTERN, password=password))
+
+
+class PasswordResetTests(TestCase):
+    def setUp(self):
+        url = reverse('password_reset')
+        self.response = self.client.get(url)
+
+    def test_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
