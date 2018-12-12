735df7346c2994b692edf0c997530864470cbacd
caco <crns13@gmail.com>
2018-12-12 16:45:17 -0200

    NES-898 Import Experiment (without ethics committee file yet) and Research Project (without keywords yet)

--- patientregistrationsystem/qdc/experiment/import_experiment.py
+++ patientregistrationsystem/qdc/experiment/import_experiment.py
@@ -1,10 +1,39 @@
 import tempfile
 import zipfile
+from os import path
+
+from tablib import Dataset
+
+from experiment.admin import ResearchProjectResource, ExperimentResource
+from experiment.models import ResearchProject
 
 
-TEMP_DIR = tempfile.mkdtemp()
+def import_research_project(base_dir, file_name):
+    file_path = path.join(base_dir, file_name)
+    dataset = Dataset().load(open(file_path).read())
+    result = ResearchProjectResource().import_data(dataset, dry_run=True)
+    if not result:
+        print('Error!')  # TODO: return error
+    ResearchProjectResource().import_data(dataset)
+
+    return ResearchProject.objects.last()
 
 
-def import_experiment(zip_path):
+def import_experiment(base_dir, file_name, research_project):
+    file_path = path.join(base_dir, file_name)
+    dataset = Dataset().load(open(file_path).read())
+    dataset.append_col([research_project.id], header='research_project')
+    print(dataset.csv)  # DEBUG
+    result = ExperimentResource().import_data(dataset, dry_run=True)
+    if not result:
+        print('Error!')  # TODO: return error
+    ExperimentResource().import_data(dataset)
+
+
+def import_all(zip_path):
+    temp_dir = tempfile.mkdtemp()
     with zipfile.ZipFile(zip_path) as f:
-        f.extractall(TEMP_DIR)
+        f.extractall(temp_dir)
+
+    research_project = import_research_project(temp_dir, 'research_project.csv')
+    import_experiment(temp_dir, 'experiment.csv', research_project)
