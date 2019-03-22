07f36f9a587a124c15f6ba4b72dab47f654ea873
Éder Porto <eder.porto@hotmail.com>
2019-03-22 17:32:12 -0300

    fixing typo

--- patientregistrationsystem/qdc/experiment/import_export_model_relations.py
+++ patientregistrationsystem/qdc/experiment/import_export_model_relations.py
@@ -373,18 +373,18 @@ PRE_LOADED_PATIENT_MODEL = {
     ],
 }
 
-MODELS_WITH_FILE_FIELD = [
-    ["experiment.experiment", "ethics_committee_project_file"],
-    ["experiment.componentadditionalfile", "file"],
-    ["experiment.emgelectrodeplacement", "photo"],
-    ["experiment.eegelectrodelocalizationsystem", "map_image_file"],
-    ["experiment.genericdatacollectionfile", "file"],
-    ["experiment.additionaldatafile", "file"],
-    ["experiment.emgfile", "file"],
-    ["experiment.tmslocalizationsystem", "tms_localization_system_image"],
-    ["experiment.eegfile", "file"],
-    ["experiment.stimulus", "media_file"],
-    ["experiment.contexttree", "setting_file"],
-    ["experiment.digitalgamephasefile", "file"],
-    ["patient.examfile", "content"]
+MODELS_WITH_FILE_FIELD = {
+    ("experiment.experiment", "ethics_committee_project_file"),
+    ("experiment.componentadditionalfile", "file"),
+    ("experiment.emgelectrodeplacement", "photo"),
+    ("experiment.eegelectrodelocalizationsystem", "map_image_file"),
+    ("experiment.genericdatacollectionfile", "file"),
+    ("experiment.additionaldatafile", "file"),
+    ("experiment.emgfile", "file"),
+    ("experiment.tmslocalizationsystem", "tms_localization_system_image"),
+    ("experiment.eegfile", "file"),
+    ("experiment.stimulus", "media_file"),
+    ("experiment.contexttree", "setting_file"),
+    ("experiment.digitalgamephasefile", "file"),
+    ("patient.examfile", "content")
 }
