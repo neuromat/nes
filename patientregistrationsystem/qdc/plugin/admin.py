3ba7e9f8706de72dcebea98d9a58136dcb0241e3
carlosribas <caduribas@gmail.com>
2019-05-13 18:00:27 -0300

    Add Singleton to admin

--- patientregistrationsystem/qdc/plugin/admin.py
+++ patientregistrationsystem/qdc/plugin/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from solo.admin import SingletonModelAdmin
 from .models import RandomForests
 
-admin.site.register(RandomForests)
+admin.site.register(RandomForests, SingletonModelAdmin)
