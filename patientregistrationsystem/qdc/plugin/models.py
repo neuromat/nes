b79675e3a39cb393ebc8eab490a2e135f2b89e14
carlosribas <caduribas@gmail.com>
2019-05-14 16:17:53 -0300

    Fields not mandatory

--- patientregistrationsystem/qdc/plugin/models.py
+++ patientregistrationsystem/qdc/plugin/models.py
@@ -5,8 +5,8 @@ from survey.models import Survey
 
 
 class RandomForests(SingletonModel):
-    admission_assessment = models.ForeignKey(Survey, related_name='survey_admission')
-    surgical_evaluation = models.ForeignKey(Survey, related_name='survey_surgical')
+    admission_assessment = models.ForeignKey(Survey, related_name='survey_admission', blank=True, null=True)
+    surgical_evaluation = models.ForeignKey(Survey, related_name='survey_surgical', blank=True, null=True)
 
     class Meta:
         permissions = (
