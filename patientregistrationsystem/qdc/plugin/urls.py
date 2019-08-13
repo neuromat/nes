d27d3681a540bdc3ec7921cb5e4b031a929c94d2
caco <crns13@gmail.com>
2019-08-13 16:36:12 -0300

    NES-995 discards call-plugin approach: redirects to existent send-to-plugin view; improves test name

--- patientregistrationsystem/qdc/plugin/urls.py
+++ patientregistrationsystem/qdc/plugin/urls.py
@@ -3,5 +3,4 @@ from plugin import views
 
 urlpatterns = [
     url(r'^$', views.send_to_plugin, name='send-to-plugin'),
-    url(r'^call_plugin/(?P<user_id>\d+)/(?P<export_id>\d+)/$', views.call_plugin, name='call-plugin')
 ]
