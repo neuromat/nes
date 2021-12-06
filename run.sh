327f8b27b39fded588323d1b2599c94c782102e4
David H. Reese M <dreese17@gmail.com>
2021-12-06 10:33:28 -0300

    Refactoring scripts for local development

--- run.sh
+++ run.sh
@@ -2,12 +2,16 @@
 
 . local_environment.list
 
-docker stop nes
-docker rm nes
-docker run -dit --name nes \
-    --mount type=bind,source=$ENTRYPOINT_FILE,target=/entrypoint.sh \
-    -v $LIMESURVEY_CONFIG_DIR:/var/www/limesurvey/application/config \
-    -v $PG_DB_DIR:/var/lib/postgresql/data \
-    -v $NES_DIR:/nes \
-    -p 8080:8080 -p 8000:8000 -p 5999:5432 nes
+{
+  docker start nes
+} || {
+  docker stop nes
+  docker rm nes
+  docker run -dit --name nes \
+      --mount type=bind,source=$ENTRYPOINT_FILE,target=/entrypoint.sh \
+      -v $LIMESURVEY_CONFIG_DIR:/var/www/limesurvey/application/config \
+      -v $PG_DB_DIR:/var/lib/postgresql/data \
+      -v $NES_DIR:/nes \
+      -p 8080:8080 -p 8000:8000 -p 5999:5432 nes
+}
 docker exec -ti nes bash
