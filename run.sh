da9d8a8b28e8ab0f7f6b2feb24532d5da18493b4
David H. Reese M <dreese17@gmail.com>
2021-12-06 10:28:33 -0300

    Refactoring scripts for local development

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
+. local_environment.list
+
 docker stop nes
 docker rm nes
-# docker run -it --name nes --volume /home/dreese/Proyectos/dreese/development/FPUNA/nes-1:/nes/ -p 8080:8080 -p 8000:8000 -p 5999:5432 --entrypoint '/bin/sh' nes
-# docker run -it --name nes -v /home/dreese/Proyectos/dreese/development/FPUNA/volumes/postgres_db:/var/lib/postgresql/data -v /home/dreese/Proyectos/dreese/development/FPUNA/nes-1:/nes -p 8080:8080 -p 8000:8000 -p 5999:5432 nes
 docker run -dit --name nes \
-    --mount type=bind,source=/home/dreese/Proyectos/dreese/development/FPUNA/nes-1/entrypoint_local_development.sh,target=/entrypoint.sh \
-    -v /home/dreese/Proyectos/dreese/development/FPUNA/volumes/limesurvey:/var/www/limesurvey/application/config \
-    -v /home/dreese/Proyectos/dreese/development/FPUNA/volumes/postgres_db:/var/lib/postgresql/data \
-    -v /home/dreese/Proyectos/dreese/development/FPUNA/nes-1:/nes \
+    --mount type=bind,source=$ENTRYPOINT_FILE,target=/entrypoint.sh \
+    -v $LIMESURVEY_CONFIG_DIR:/var/www/limesurvey/application/config \
+    -v $PG_DB_DIR:/var/lib/postgresql/data \
+    -v $NES_DIR:/nes \
     -p 8080:8080 -p 8000:8000 -p 5999:5432 nes
 docker exec -ti nes bash
