3191d70af97b595834e3ea8c7be158128761d9d6
mateus <mcostat@peb.ufrj.br>
2023-08-14 18:17:32 +0000

    entrypoint

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -7,11 +7,11 @@ PGDATA=${PGDATA:-"/var/lib/postgresql/data"}
 ## SYSTEM OPTIONS
 LIMESURVEY_HOST=${LIMESURVEY_HOST:-"localhost"}
 LIMESURVEY_PORT=${LIMESURVEY_PORT:-"80"}
-LIMESURVEY_DIR="$LIMESURVEY_DIR"
+LIMESURVEY_DIR=$LIMESURVEY_DIR
 APACHE2_CONF_DIR="${LIMESURVEY_DIR}/application/config"
 ## CONFIG.PHP OPTIONS
-LIMESURVEY_DB_TYPE=${LIMESURVEY_DB_TYPE:-'psql'}
-LIMESURVEY_DB_HOST=${LIMESURVEY_DB_HOST:-"db"}
+LIMESURVEY_DB_TYPE=${LIMESURVEY_DB_TYPE:-'pgsql'}
+LIMESURVEY_DB_HOST=${LIMESURVEY_DB_HOST:-"localhost"}
 LIMESURVEY_DB_PORT=${LIMESURVEY_DB_PORT:-"5432"}
 LIMESURVEY_DB=${LIMESURVEY_DB:-"limesurvey_db"}
 LIMESURVEY_DB_TABLE_PREFIX=${LIMESURVEY_DB_TABLE_PREFIX:-"lime_"}
@@ -39,7 +39,7 @@ NES_ADMIN_PASSWORD=${NES_ADMIN_PASSWORD:-"nes_admin_password"}
 ### DB
 #### External database settings
 NES_DB_TYPE=${NES_DB_TYPE:-'pgsql'}
-NES_DB_HOST=${NES_DB_HOST:-'db'}
+NES_DB_HOST=${NES_DB_HOST:-'localhost'}
 NES_DB_PORT=${NES_DB_PORT:-'5432'}
 NES_DB=${NES_DB:-'nes_db'}
 NES_DB_USER=${NES_DB_USER:-'nes_db_user'}
@@ -62,26 +62,6 @@ fi
 
 
 # LIMESURVEY SETUP ####################################################
-
-if [ -f "${LIMESURVEY_DIR}"/application/config/config.php ]
-then
-	echo "INFO: LimeSurvey config.php already provisioned"
-else
-	cd "$LIMESURVEY_DIR"
-	echo "INFO: Creating LimeSurvey configuration"
-	cp application/config/config-sample-pgsql.php application/config/config.php
-	sed -i "s#\('connectionString' => \).*,\$#\\1'pgsql:host=${LIMESURVEY_DB_HOST};port=${LIMESURVEY_DB_PORT};dbname=${LIMESURVEY_DB};',#g" application/config/config.php
-	sed -i "s#\('username' => \).*,\$#\\1'${LIMESURVEY_DB_USER}',#g" application/config/config.php
-	sed -i "s#\('password' => \).*,\$#\\1'${LIMESURVEY_DB_PASSWORD}',#g" application/config/config.php
-	sed -i "s#\('charset' => \).*,\$#\\1'${LIMESURVEY_DB_CHARSET}',#g" application/config/config.php
-	sed -i "s#\('tablePrefix' => \).*,\$#\\1'${LIMESURVEY_DB_TABLE_PREFIX}',#g" application/config/config.php
-	sed -i "s#\('urlFormat' => \).*,\$#\\1'${LIMESURVEY_URL_FORMAT}',#g" application/config/config.php
-fi
-
-# Makes sure that JSON-RPC interface will be available
-cd "$LIMESURVEY_DIR"
-sed -i "s#\(\$config\['RPCInterface'\]\ =\ \).*;\$#\\1'json';#g" application/config/config-defaults.php
-
 if [ -f "${APACHE2_CONF_DIR}"/httpd.conf ]
 then
 	echo "INFO: LimeSurvey-Apache configuration already provisioned"
@@ -167,16 +147,70 @@ else
 		</IfModule>
 
 		TypesConfig /etc/apache2/mime.types
+		
 		AddType application/x-compress .Z
 		AddType application/x-gzip .gz .tgz
 		AddType application/x-httpd-php php
 		AddType application/x-httpd-php-source phps
 
 		<IfModule mime_magic_module>
-		MIMEMagicFile /etc/apache2/magic
+			MIMEMagicFile /etc/apache2/magic
 		</IfModule>
 	EOF
 	chown -R apache:apache "$APACHE2_CONF_DIR"
+fi
+
+while ! nc -z "$LIMESURVEY_DB_HOST" "$LIMESURVEY_DB_PORT"
+do
+	sleep 0.2
+done
+
+if [ -f "${LIMESURVEY_DIR}"/application/config/config.php ]
+then
+	echo "INFO: LimeSurvey config.php already provisioned"
+else
+	cd "$LIMESURVEY_DIR"
+	echo "INFO: Creating LimeSurvey configuration"
+
+	if [ $LIMESURVEY_DB_TYPE = 'mysql' ]; then
+        echo 'INFO: Using MySQL configuration'
+        LIMESURVEY_DB_CHARSET=${LIMESURVEY_DB_CHARSET:-'utf8mb4'}
+        cp application/config/config-sample-mysql.php application/config/config.php
+    elif [ $LIMESURVEY_DB_TYPE = 'pgsql' ]; then
+        echo 'INFO: Using PostgreSQL configuration'
+        LIMESURVEY_DB_CHARSET=${LIMESURVEY_DB_CHARSET:-'utf8'}
+        cp application/config/config-sample-pgsql.php application/config/config.php
+    else
+    	echo 'Error: unrecognized LIMESURVEY_DB_TYPE: "$LIMESURVEY_DB_TYPE"' 
+    	exit 1
+    fi
+
+	echo 'INFO: Using TCP connection'
+	sed -i "s#\('connectionString' => \).*,\$#\\1'pgsql:host=${LIMESURVEY_DB_HOST};port=${LIMESURVEY_DB_PORT};dbname=${LIMESURVEY_DB};',#g" application/config/config.php
+	sed -i "s#\('username' => \).*,\$#\\1'${LIMESURVEY_DB_USER}',#g" application/config/config.php
+	sed -i "s#\('password' => \).*,\$#\\1'${LIMESURVEY_DB_PASSWORD}',#g" application/config/config.php
+	sed -i "s#\('charset' => \).*,\$#\\1'${LIMESURVEY_DB_CHARSET}',#g" application/config/config.php
+	sed -i "s#\('tablePrefix' => \).*,\$#\\1'${LIMESURVEY_DB_TABLE_PREFIX}',#g" application/config/config.php
+	sed -i "s#\('urlFormat' => \).*,\$#\\1'${LIMESURVEY_URL_FORMAT}',#g" application/config/config.php
+
+	# Makes sure that JSON-RPC interface will be available
+	sed -i "s#\(\$config\['RPCInterface'\]\ =\ \).*;\$#\\1'json';#g" application/config/config-defaults.php
+
+
+	# Check if LIMESURVEY_DB_PASSWORD is set
+    if [ -z "$LIMESURVEY_DB_PASSWORD" ]; then
+        echo >&2 'Error: Missing LIMESURVEY_DB_PASSWORD'
+        exit 1
+    fi
+
+    # Check if LIMESURVEY_DB_PASSWORD is set
+    if [ -z "$LIMESURVEY_ADMIN_PASSWORD" ]; then
+        echo >&2 'Error: Missing LIMESURVEY_ADMIN_PASSWORD'
+        exit 1
+    fi
+
+    echo 'INFO: Running console.php install'
+    su apache -c php application/commands/console.php install $LIMESURVEY_ADMIN_USER $LIMESURVEY_ADMIN_PASSWORD $LIMESURVEY_ADMIN_NAME $LIMESURVEY_ADMIN_EMAIL
 fi
 
 # NES SETUP ####################################################
