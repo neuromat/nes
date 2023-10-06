236dee0a51398614487444639bbac24e584e6f05
Mateus <mcostat@outlook.com>
2023-10-06 18:12:07 +0000

    eeg images

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -113,7 +113,11 @@ except:
     echo "	INFO: populate_history"
     python3 -u manage.py populate_history --auto || true
     
-    
+
+    cp -r $NES_PROJECT_PATH/static/imgs/International_10-10_system_for_EEG.png $NES_PROJECT_PATH/media/eeg_electrode_system_files/2/International_10-10_system_for_EEG.png
+    cp -r $NES_PROJECT_PATH/static/imgs/International_10-20_system_for_EEG.png $NES_PROJECT_PATH/media/eeg_electrode_system_files/3/International_10-20_system_for_EEG.png
+    cp -r $NES_PROJECT_PATH/static/imgs/128_channel_HCGSN_v.1.0.png $NES_PROJECT_PATH/media/eeg_electrode_system_files/1/128_channel_HCGSN_v.1.0.png
+
     rm /tmp/create_superuser.py
     
     # If NES was installed from a release it won"t have a .git directory
